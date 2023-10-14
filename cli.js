9f42f2d44c8c7041b6b681fac1e3607a881736ee
Israel Sant'Anna <israel.santanna@gmail.com>
2023-10-14 14:23:47 -0300

    ♻️ Move `createComponentFiles` to `createComponent`

--- cli.js
+++ cli.js
@@ -16,40 +16,27 @@ program.version("0.0.2");
  * @param {"js" | "ts"} type
  * @returns void
  */
-function createComponentFiles(componentName, type) {
-  const folder = componentName;
-  const templates = {
-    ts: tsTemplate,
-    js: jsTemplate,
-  };
-
-  const files = [
-    { name: `${componentName}.${type === "ts" ? "tsx" : "jsx"}`, content: templates[type].component },
-    { name: `${componentName}.spec.${type === "ts" ? "tsx" : "jsx"}`, content: templates[type].spec },
-    { name: `${componentName}.css`, content: templates[type].css },
-  ];
-
-  fs.mkdirSync(folder);
-  files.forEach((file) => {
-    const filePath = path.join(folder, file.name);
-    fs.writeFileSync(filePath, file.content);
-  });
-
-  console.log("Folder and files created successfully!");
-}
-
-/**
- *
- * Creates a component file as `componentName`.`type`
- * Creates a test for the created component
- *
- * @param {string} componentName
- * @param {"js" | "ts"} type
- * @returns void
- */
 function createComponent(componentName, type) {
   try {
-    createComponentFiles(componentName, type);
+    const folder = componentName;
+    const templates = {
+      ts: tsTemplate,
+      js: jsTemplate,
+    };
+
+    const files = [
+      { name: `${componentName}.${type === "ts" ? "tsx" : "jsx"}`, content: templates[type].component },
+      { name: `${componentName}.spec.${type === "ts" ? "tsx" : "jsx"}`, content: templates[type].spec },
+      { name: `${componentName}.css`, content: templates[type].css },
+    ];
+
+    fs.mkdirSync(folder);
+    files.forEach((file) => {
+      const filePath = path.join(folder, file.name);
+      fs.writeFileSync(filePath, file.content);
+    });
+
+    console.log("Folder and files created successfully!");
   } catch (error) {
     console.error(`Something went wrong: ${error}`);
   }
